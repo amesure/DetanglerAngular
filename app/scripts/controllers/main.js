88eb7ae5ab4eb486229dd807b1d608d540329dfa
Anne-laure Mesure <bluewolf33@hotmail.fr>
2015-09-06 23:00:24 +0200

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name detanglerAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the detanglerAngularApp
+ */
+angular.module('detanglerAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
